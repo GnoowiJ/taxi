12971c264cff3b6a324325db261755acaa1c1c08
웅's <qowldnd002@naver.com>
2024-09-18 19:48:24 +0900

    09/18 main

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import './css/style.css';
+import './css/home.css';
 import Root from "./Root";
 import Home from "./pages/Home";
 
--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -6,29 +6,29 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 export default function Header () {
   return (
     <div className='header'>
-      <div className='header-box'>
-        <h1 className='header-logo'>
-          {/* <img className='header-logoimg' src="https://www.intltaxi.co.kr/assets/images/logo.svg" alt="" /> */}
-        </h1>
-        <ul className="navbox">
-          <li>ABOUT</li>
-          <li>LINE-UP</li>
-          <li>SERVICE</li>
-          <li>PRICE</li>
-          <li>RESERVATION</li>
-        </ul>
-        <div className='header-select-box'>
-          <div className='select-box'>
-            <span>KOR</span>
-            <FontAwesomeIcon icon={faChevronDown} style={{"color": "yellow"}}/>
-          </div>
-          <ul className='header-select' style={{"display": "none"}}>
-            <li>KOR</li>
-            <li>ENG</li>
-            <li>JPN</li>
-            <li>CHN</li>
-            <li>KOR</li>
+      <div className='header-img'>
+        <div className='header-box'>
+          <h1 className='header-logo' />
+          <ul className="navbox">
+            <li>ABOUT</li>
+            <li>LINE-UP</li>
+            <li>SERVICE</li>
+            <li>PRICE</li>
+            <li>RESERVATION</li>
           </ul>
+          <div className='header-select-box'>
+            <div className='select-box'>
+              <span>KOR</span>
+              <FontAwesomeIcon icon={faChevronDown} style={{"color": "yellow"}}/>
+            </div>
+            <ul className='header-select' style={{"display": "none"}}>
+              <li>KOR</li>
+              <li>ENG</li>
+              <li>JPN</li>
+              <li>CHN</li>
+              <li>KOR</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
